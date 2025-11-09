0e3b10ded57300b18c52a1cb01d7b1bce5048faa
Jishan <mdjishanprodhan132@gamil.com>
2025-11-09 10:09:57 -0600

    remove consol log

--- js/login.js
+++ js/login.js
@@ -1,6 +1,5 @@
 document.getElementById('login-btn').addEventListener('click',function(event){
     event.preventDefault();
-    console.log("login btn clicked");
     const inputNumber = getNumberValueFromInputFild('input-number');
     const inputPin = getNumberValueFromInputFild('input-pin');
     if(isNaN(inputNumber)) {
@@ -8,7 +7,6 @@ document.getElementById('login-btn').addEventListener('click',function(event){
     }
     /// this is not proper way the proper way is using fatch data forom server side 
     if((inputNumber === 123456 ) && (inputPin === 1234)){
-        console.log('number thik ache');
         window.location.href = './home.html'
     }
     console.log("log houtase ")
